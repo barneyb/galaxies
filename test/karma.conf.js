5748704a62b8ddcefc3c799e2de883e5a3c03239
barneyb <bboisvert@gmail.com>
2014-05-26 11:10:43 -0700

    updated test file paths based on the coffee/js split hierarchy

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -8,7 +8,7 @@ module.exports = function(config){
       'app/bower_components/angular-route/angular-route.js',
       'app/bower_components/angular-mocks/angular-mocks.js',
       'app/js/**/*.js',
-      'test/unit/**/*.js'
+      'test/js/unit/**/*.js'
     ],
 
     autoWatch : true,
